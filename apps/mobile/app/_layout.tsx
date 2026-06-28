94f77d19df903af61b4de367c356ba3386b6e8db
Pavle <strbacpavlejob@gmail.com>
2026-06-28 16:53:40 +0200

    Change default title and favicon

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -12,7 +12,7 @@ import * as NavigationBar from "expo-navigation-bar";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-import { StatusBar } from "react-native";
+import { Platform, StatusBar } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import {
   SafeAreaProvider,
@@ -50,6 +50,12 @@ const MainStack = () => {
     NavigationBar.setBackgroundColorAsync(background).catch(console.warn);
     NavigationBar.setButtonStyleAsync("light");
   }, [background]);
+
+  useEffect(() => {
+    if (Platform.OS === "web") {
+      document.title = "Tulum";
+    }
+  }, []);
 
   return (
     <>
