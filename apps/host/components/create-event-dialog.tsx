844ca1dc4045bfd7e55c30600ade48288dd6e4a5
Pavle <strbacpavlejob@gmail.com>
2026-09-10 21:01:46 +0200

    Revert create event dialog set value

--- apps/host/components/create-event-dialog.tsx
+++ apps/host/components/create-event-dialog.tsx
@@ -220,7 +220,6 @@ export function CreateEventDialog({
     if (!file) {
       setImageFile(null);
       setPicturePreview(null);
-      setValue("picture_url", "");
       return;
     }
 
@@ -272,7 +271,6 @@ export function CreateEventDialog({
           start_date_time: data.start_date_time,
           end_date_time: data.end_date_time,
           tags: data.tags,
-          picture_url: data.picture_url || event.picture_url || undefined,
           status: data.status,
         };
 
@@ -292,7 +290,6 @@ export function CreateEventDialog({
           start_date_time: data.start_date_time,
           end_date_time: data.end_date_time,
           tags: data.tags,
-          picture_url: data.picture_url || undefined,
           status: data.status,
         };
 
